a3c2b3b48e25d29ec6d74db3476cbdb414cf9022
Tekmily <yusuftekmil.yt@gmail.com>
2022-08-16 13:01:20 +0300

    v2

--- src/Deneme.java
+++ src/Deneme.java
@@ -2,6 +2,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("1.denem");
         System.out.println("2.denem");
+        System.out.println("3.denem");
 
     }
 }
